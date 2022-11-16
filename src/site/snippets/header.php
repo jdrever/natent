57e84d50ea762c74511b1f721bb073319e3af08b
James Drever <james@careful.digital>
2022-11-16 10:07:17 +0000

    Removing Careful Digital content

--- src/site/snippets/header.php
+++ src/site/snippets/header.php
@@ -7,7 +7,7 @@
           content="Natural Entrepeneurs platform: biomimicry for schools">
     <meta name="author" content="Careful Digital">
     <title></title>
-    <link rel="stylesheet" type="text/css" href="/css/custom.css">
+    <link rel="stylesheet" type="text/css" href="<?=url('/assets/css/custom.css')?>">
     <link rel="stylesheet"
           type="text/css"
           href="https://natent.eu/wp-content/themes/carefulcollab/css//mpcourses/classroom.css">
@@ -518,4 +518,73 @@
     </style>
   </head>
   <body style="background-color: #F8F5F4;">
-    {% include "top-menu.njk" %}
+
+  <header>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+      <button class="navbar-toggler"
+              type="button"
+              data-mdb-toggle="collapse"
+              data-mdb-target="#navbarExample01"
+              aria-controls="navbarExample01"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="site-logo d-flex align-items-center col-md-3 mb-2 mb-md-0 text-decoration-none">
+        <a href="/" class="d-flex align-items-center text-decoration-none">
+          <img class="img-fluid m-1" src="/images/natent-logo.svg" width="50"/>
+          Natural
+          <br>
+          Entrepreneurs
+        </a>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarExample01">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li>
+            <a href="/" class="nav-link px-2">Home</a>
+          </li>
+          <li>
+            <a href="#" class="nav-link px-2">About the Project</a>
+          </li>
+          <li>
+            <a href="/for-teachers/" class="nav-link px-2">For Teachers</a>
+          </li>
+          <li>
+            <a href="/for-students/" class="nav-link px-2">For Students</a>
+          </li>
+          <li>
+            <a href="/platform/" class="nav-link px-2">The Platform</a>
+          </li>
+          <li>
+            <a href="/contact/" class="nav-link px-2">Contact Us</a>
+          </li>
+        </ul>
+        <div class="col-md-3 text-end">
+          <select name="lang_choice_1" id="lang_choice_1">
+            <option value="de">
+              Deutsch
+            </option>
+            <option value="lv">
+              Latviešu valoda
+            </option>
+            <option value="ro">
+              Română
+            </option>
+            <option value="nl">
+              Nederlands
+            </option>
+            <option value="en" selected='selected'>
+              English
+            </option>
+            <option value="hu">
+              Magyar
+            </option>
+          </select>
+        </div>
+      </div>
+    </div>
+  </nav>
+</header>
+
