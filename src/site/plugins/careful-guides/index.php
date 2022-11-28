859e9e7a2a56d6bb4c1e53c59f0864695879e776
James Drever <james@careful.digital>
2022-11-28 15:14:28 +0000

    adding guideContent field blueprint

--- src/site/plugins/careful-guides/index.php
+++ src/site/plugins/careful-guides/index.php
@@ -6,10 +6,11 @@ Kirby::plugin(
       'blocks/activity' => __DIR__ . '/blueprints/blocks/activity.yml',
       'blocks/image' => __DIR__ . '/blueprints/blocks/image.yml',
       'blocks/heading' => __DIR__ . '/blueprints/blocks/heading.yml',
+      'fields/guideContent' => __DIR__ . '/blueprints/fields/guideContent.yml'
     ],
     'snippets' => [
       'blocks/image' => __DIR__ . '/snippets/blocks/image.php',
       'blocks/heading' => __DIR__ . '/snippets/blocks/heading.php',
-    ],
+    ]
   ]
 );
