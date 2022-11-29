8fd5a2c2109d2d8612b04f101ad7e7b0c27fa2eb
James Drever <james@careful.digital>
2022-11-29 16:40:12 +0000

    git ignore

--- src/site/plugins/k3-trevor-view/config/translations/it.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'trevor.heading' => 'Traduzioni',
-    'trevor.startinfo' => 'Seleziona una variable a sinistra per iniziare a tradurre.',
-    'trevor.addkey' => 'Inserisci il nome della variabile.',
-    'trevor.payoff' => 'nessuno treduci voriabili meglio.'
-];
