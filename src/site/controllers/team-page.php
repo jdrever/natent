2441c836fea7765b1ba9b0a63ad5ecdce442d7ea
James Drever <james@careful.digital>
2022-11-30 17:01:13 +0000

    starting to add team page

---
+++ src/site/controllers/team-page.php
@@ -0,0 +1,61 @@
+<?php
+use carefulcollab\helpers as helpers;
+
+return function($kirby, $pages, $page, $site) 
+{
+    $platform = $kirby->controller('platform' , compact('page', 'pages', 'kirby'));
+    $team=$platform['team'];
+
+    if($kirby->request()->is('POST')) 
+    {
+        $focusId = get('focusId');
+        $bespokeChallenge = htmlspecialchars(get('bespokeChallenge',''));
+        $challengeId = get('challengeId');
+        $result=helpers\DataHelper::updateTeamChallenge($team['user_id'], $focusId, $challengeId,$bespokeChallenge); 
+
+        return $kirby->controller('result' , compact('page', 'site', 'result'));
+
+    }
+    else if (get('topicId'))
+    {
+        $topicId=get('topicId');
+        $topic=helpers\DataHelper::getAreaToInvestigate($topicId);
+        $challenges=helpers\DataHelper::getChallengesInArea($team['user_id'], $topicId);
+        $bespokeChallenge='';
+        if ($team['bespoke_challenge']==1) $bespokeChallenge=$team['challenge'];
+
+        $teams = helpers\DataHelper::getTeamsByAreaId($topicId);
+
+        return [
+            'topic' => $topic,
+            'challenges' => $challenges,
+            'teams' => $teams,
+            'showTopics' => false,
+            'showChallenges' => true,
+            'bespokeChallenge' => $bespokeChallenge,
+        ];
+    }
+    else
+    {
+        $areas=helpers\DataHelper::getAreasWithAvailableChallenges($team['user_id']);
+
+        $currentLang=$kirby->language()->code();
+    
+        $imageFileEnding=".png";
+        if ($currentLang=='lv')
+            $imageFileEnding='-lv.jpg';
+        if ($currentLang=='nl')
+            $imageFileEnding='-nl.png';
+        if ($currentLang=='de')
+            $imageFileEnding='-de.jpg';
+        if ($currentLang=='ro')
+            $imageFileEnding='-ro.png';
+
+        return [
+            'showTopics' => true,
+            'showChallenges' => false,
+            'areas' => $areas,
+            'imageFileEnding' => $imageFileEnding
+        ];
+    };
+};
