3f1a93691b4179fa74d578f44a1d2fee9aaafa54
James Drever <james@careful.digital>
2022-12-05 11:16:07 +0000

    adding accordion for comments/appreciations

--- src/site/templates/platform-home.php
+++ src/site/templates/platform-home.php
@@ -17,9 +17,8 @@
           <figcaption class="figure-caption">
             1. Define
           </figcaption>
-          <img class="figure-img img-fluid rounded"
-               src="/assets/images/define.png"
-               alt="'Dragonfly' by Rhubarble is marked with CC BY-NC 2.0. To view the terms, visit https://creativecommons.org/licenses/by-nc/2.0/?ref=openverse">
+          <img class="figure-img img-fluid rounded" src="/assets/images/define.png"
+            alt="'Dragonfly' by Rhubarble is marked with CC BY-NC 2.0. To view the terms, visit https://creativecommons.org/licenses/by-nc/2.0/?ref=openverse">
         </figure>
       </a>
       <br>
@@ -28,9 +27,8 @@
           <figcaption class="figure-caption">
             2. Discover
           </figcaption>
-          <img class="figure-img img-fluid rounded"
-               src="/assets/images/discover.jpeg"
-               alt="'Bracket Fungi.(Basidiomycotina)' by Bernard Spragg is marked with CC0 1.0.">
+          <img class="figure-img img-fluid rounded" src="/assets/images/discover.jpeg"
+            alt="'Bracket Fungi.(Basidiomycotina)' by Bernard Spragg is marked with CC0 1.0.">
         </figure>
       </a>
       <br>
@@ -39,9 +37,8 @@
           <figcaption class="figure-caption">
             3. Create
           </figcaption>
-          <img class="figure-img img-fluid rounded"
-               src="/assets/images/create.jpeg"
-               alt="'Nature bird' by @Doug88888 is marked with CC BY-NC-SA 2.0. To view the terms, visit https://creativecommons.org/licenses/by-nc-sa/2.0/?ref=openverse">
+          <img class="figure-img img-fluid rounded" src="/assets/images/create.jpeg"
+            alt="'Nature bird' by @Doug88888 is marked with CC BY-NC-SA 2.0. To view the terms, visit https://creativecommons.org/licenses/by-nc-sa/2.0/?ref=openverse">
         </figure>
       </a>
       <br>
@@ -50,31 +47,29 @@
           <figcaption class="figure-caption">
             4. Communicate
           </figcaption>
-          <img class="figure-img img-fluid rounded"
-               src="/assets/images/communicate.jpeg"
-               alt="'Oregano Orejon' by tatadbb is marked with CC BY-SA 2.0. To view the terms, visit https://creativecommons.org/licenses/by-sa/2.0/?ref=openverse">
+          <img class="figure-img img-fluid rounded" src="/assets/images/communicate.jpeg"
+            alt="'Oregano Orejon' by tatadbb is marked with CC BY-SA 2.0. To view the terms, visit https://creativecommons.org/licenses/by-sa/2.0/?ref=openverse">
         </figure>
       </a>
       <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-        <button class="btn btn-primary btn-sm m-2"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseImageCredits"
-                aria-expanded="false"
-                aria-controls="collapseImageCredits">
+        <button class="btn btn-primary btn-sm m-2" type="button" data-bs-toggle="collapse"
+          data-bs-target="#collapseImageCredits" aria-expanded="false" aria-controls="collapseImageCredits">
           See Image Credits
         </button>
         <div class="collapse" id="collapseImageCredits">
           <p>
             <small>
               <b>Define:</b>
-              'Dragonfly' by Rhubarble is marked with CC BY-NC 2.0. To view the terms, visit https://creativecommons.org/licenses/by-nc/2.0/?ref=openverse
+              'Dragonfly' by Rhubarble is marked with CC BY-NC 2.0. To view the terms, visit
+              https://creativecommons.org/licenses/by-nc/2.0/?ref=openverse
               <b>Discover:</b>
               'Bracket Fungi.(Basidiomycotina)' by Bernard Spragg is marked with CC0 1.0.
               <b>Create:</b>
-              'Nature bird' by @Doug88888 is marked with CC BY-NC-SA 2.0. To view the terms, visit https://creativecommons.org/licenses/by-nc-sa/2.0/?ref=openverse
+              'Nature bird' by @Doug88888 is marked with CC BY-NC-SA 2.0. To view the terms, visit
+              https://creativecommons.org/licenses/by-nc-sa/2.0/?ref=openverse
               <b>Communications:</b>
-              'Oregano Orejon' by tatadbb is marked with CC BY-SA 2.0. To view the terms, visit https://creativecommons.org/licenses/by-sa/2.0/?ref=openverse
+              'Oregano Orejon' by tatadbb is marked with CC BY-SA 2.0. To view the terms, visit
+              https://creativecommons.org/licenses/by-sa/2.0/?ref=openverse
             </small>
           </p>
         </div>
@@ -86,90 +81,118 @@
         <i class="bi bi-star"></i> 220
       </p>
       <p>
-        You earn collaboration points by completing Phases, adding resources to the Creative Commons and appreciating and commenting on other teams
+        You earn collaboration points by completing Phases, adding resources to the Creative Commons and appreciating
+        and commenting on other teams
       </p>
       <div class="container">
         <div class="row">
           <div class="col bg-light d-none d-lg-block">
-            <h5>Latest Comments</h5>
-            <p class="p-1 m-0 border">
-              <small>
-                <i class="bi bi-chat-fill"></i>
-                on your Commons Resource by
-                <a href="/other-team-page/?teamId=91">BioBoly</a>
-                <br>
-                <i class="bi bi-quote"></i>
-                <i>Very helpful website</i>
-                <i class="bi bi-three-dots"></i>
-              </small>
-            </p>
-            <p class="p-1 m-0 border">
-              <small>
-                <i class="bi bi-chat-fill"></i>
-                on your Commons Resource by
-                <a href="/other-team-page/?teamId=78">PedaCol1</a>
-                <br>
-                <i class="bi bi-quote"></i>
-                <i>I liked so much the </i>
-                <i class="bi bi-three-dots"></i>
-              </small>
-            </p>
-            <p class="p-1 m-0 border">
-              <small>
-                <i class="bi bi-chat-fill"></i>
-                on your Commons Resource by
-                <a href="/other-team-page/?teamId=78">PedaCol1</a>
-                <br>
-                <i class="bi bi-quote"></i>
-                <i>It’s very helpfull</i>
-                <i class="bi bi-three-dots"></i>
-              </small>
-            </p>
-            <a href="https://natent.eu/en/view-all-comments-received/">View all Comments received</a>
-          </div>
-          <div class="col bg-light d-none d-lg-block">
-            <h5>Latest Appreciations</h5>
-            <p class="p-1 m-0 border">
-              <small>
-                <i class="bi bi-stars"></i>
-                of your Team Challenge by
-                <a href="/other-team-page/?teamId=117">Eichhörnchen</a>
-              </small>
-            </p>
-            <p class="p-1 m-0 border">
-              <small>
-                <i class="bi bi-stars"></i>
-                of your Commons Resource by
-                <a href="/other-team-page/?teamId=65">Refkoll 1</a>
-              </small>
-            </p>
-            <p class="p-1 m-0 border">
-              <small>
-                <i class="bi bi-stars"></i>
-                of your Function by
-                <a href="/other-team-page/?teamId=78">PedaCol1</a>
-              </small>
-            </p>
+            <div class="accordion accordion-flush" id="accordionCommentsAppreciations">
+              <div class="accordion-item">
+                <h2 class="accordion-comments" id="headingComments">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseComments" aria-expanded="false" aria-controls="collapseComments">
+                    See Latest Comments <span class="badge text-bg-primary m-2">3</span>
+                  </button>
+                </h2>
+                <div id="collapseComments" class="accordion-collapse collapse" aria-labelledby="headingComments">
+                  <div class="accordion-body">
+                    <p class="p-1 border">
+                      <small>
+                        <i class="bi bi-chat-fill"></i>
+                        on your Commons Resource by
+                        <a href="/other-team-page/?teamId=91">BioBoly</a>
+                        <br>
+                        <i class="bi bi-quote"></i>
+                        <i>Very helpful website</i>
+                        <i class="bi bi-three-dots"></i>
+                      </small>
+                    </p>
+                    <p class="p-1 border">
+                      <small>
+                        <i class="bi bi-chat-fill"></i>
+                        on your Commons Resource by
+                        <a href="/other-team-page/?teamId=78">PedaCol1</a>
+                        <br>
+                        <i class="bi bi-quote"></i>
+                        <i>I liked so much the </i>
+                        <i class="bi bi-three-dots"></i>
+                      </small>
+                    </p>
+                    <p class="p-1 border">
+                      <small>
+                        <i class="bi bi-chat-fill"></i>
+                        on your Commons Resource by
+                        <a href="/other-team-page/?teamId=78">PedaCol1</a>
+                        <br>
+                        <i class="bi bi-quote"></i>
+                        <i>It’s very helpfull</i>
+                        <i class="bi bi-three-dots"></i>
+                      </small>
+                    </p>
+                    <a href="https://natent.eu/en/view-all-comments-received/" class="btn btn-sm btn-secondary">View all
+                      Comments received</a>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-appreciations" id="headingAppreciations">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapseAppreciations" aria-expanded="false"
+                      aria-controls="collapseAppreciations">
+                      See Latest Appreciations<span class="badge text-bg-primary m-2">3</span>
+                    </button>
+                  </h2>
+                  <div id="collapseAppreciations" class="accordion-collapse collapse"
+                    aria-labelledby="headingAppreciations">
+                    <div class="accordion-body">
+                      <p class="p-1 border">
+                        <small>
+                          <i class="bi bi-stars"></i>
+                          of your Team Challenge by
+                          <a href="/other-team-page/?teamId=117">Eichhörnchen</a>
+                        </small>
+                      </p>
+                      <p class="p-1 border">
+                        <small>
+                          <i class="bi bi-stars"></i>
+                          of your Commons Resource by
+                          <a href="/other-team-page/?teamId=65">Refkoll 1</a>
+                        </small>
+                      </p>
+                      <p class="p-1 border">
+                        <small>
+                          <i class="bi bi-stars"></i>
+                          of your Function by
+                          <a href="/other-team-page/?teamId=78">PedaCol1</a>
+                        </small>
+                      </p>
+                      <a href="https://natent.eu/en/view-all-comments-received/" class="btn btn-sm btn-secondary">View
+                        all
+                        Appreciations received</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <h2>Have a look around!</h2>
+            <p>You can always get back to your current Phase by clicking on the RESUME YOUR LEARNING button</p>
+            <h3>
+              <i class="bi bi-person-heart"></i>TEAM PAGE
+            </h3>
+            <p>View how your team is getting on</p>
+            <h3>
+              <i class="bi bi-search-heart"></i>OTHER TEAMS
+            </h3>
+            <p>Browse other teams pages to get ideas and appreciate their work</p>
+            <h3>
+              <i class="bi bi-cc-circle-fill"></i>CREATIVE COMMONS
+            </h3>
+            <p>Find useful resources</p>
           </div>
         </div>
       </div>
-      <br>
-      <h2>Have a look around!</h2>
-      <p>You can always get back to your current Phase by clicking on the RESUME YOUR LEARNING button</p>
-      <h3>
-        <i class="bi bi-person-heart"></i>TEAM PAGE
-      </h3>
-      <p>View how your team is getting on</p>
-      <h3>
-        <i class="bi bi-search-heart"></i>OTHER TEAMS
-      </h3>
-      <p>Browse other teams pages to get ideas and appreciate their work</p>
-      <h3>
-        <i class="bi bi-cc-circle-fill"></i>CREATIVE COMMONS
-      </h3>
-      <p>Find useful resources</p>
     </div>
   </div>
 </div>
-
 <?php snippet('footer') ?>
