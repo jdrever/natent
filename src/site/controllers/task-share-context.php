179b91029391ddf28bbe52c69f31743793503f48
tomdrever <tomdr1001@outlook.com>
2022-12-06 17:12:22 +0000

    Make files for Share Context Task

---
+++ src/site/controllers/task-share-context.php
@@ -0,0 +1,25 @@
+<?php
+use carefulcollab\helpers as helpers;
+return function($kirby, $pages, $page, $site) {
+
+    $platform = $kirby->controller('platform' , compact('page', 'pages', 'kirby'));
+    $team=$platform['team'];
+
+    
+    if($kirby->request()->is('POST')) 
+    {
+        //$description = htmlspecialchars(get('description'));
+        //$skills = implode(',', get('skills',''));
+        //$result=helpers\DataHelper::updateTeamProfile($team['id'], $description, $skills);
+
+        //return $kirby->controller('result' , compact('page', 'site', 'result'));
+    }
+    else
+    {
+        //$skills=helpers\DataHelper::getSkillsets();
+        //return A::merge($platform, [
+        //    'skills' => $skills,
+        //    'showForm' => true
+        //]);
+    }
+};
