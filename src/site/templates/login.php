cf672181b82375a43fed08471c10278548d286dc
James Drever <james@careful.digital>
2022-12-08 08:44:28 +0000

    adding login link/styling login page

--- src/site/templates/login.php
+++ src/site/templates/login.php
@@ -1,23 +1,34 @@
 <?php snippet('header') ?>
+<?php snippet('platform-menu') ?>
+<?php snippet('show-status') ?>
 
-<h1><?= $page->title()->html() ?></h1>
+<div class="container my-4">
+  <h1 class="pb-2 border-bottom"><i class="bi bi-door-open-fill"></i> <?=$page->title()->html()?></h1>
+  <?php if($error): ?>
+  <div class="alert"><?= $page->alert()->html() ?></div>
+  <?php endif ?>
 
-<?php if($error): ?>
-<div class="alert"><?= $page->alert()->html() ?></div>
-<?php endif ?>
+    <form method="post" action="<?= $page->url() ?>">
+      <fieldset>
+          <p class="required"><?=$page->requiredFieldsLabel()?></p>
+          <ol class="list-unstyled">
+              <li class="mb-3">
+                  <label for="email" class="form-label"><?= $page->username()->html() ?>:
+              </label>
+                  <input type="text" name="email" required="required" aria-required="true" class="form-control" value="<?= get('email') ? esc(get('email'), 'attr') : '' ?>">
+              </li>
+              <li class="mb-3">
+                  <label for="password" class="form-label">
+                  <?= $page->password()->html() ?>:
 
-<form method="post" action="<?= $page->url() ?>">
-  <div>
-    <label for="email"><?= $page->username()->html() ?></label>
-    <input type="email" id="email" name="email" value="<?= get('email') ? esc(get('email'), 'attr') : '' ?>">
-  </div>
-  <div>
-    <label for="password"><?= $page->password()->html() ?></label>
-    <input type="password" id="password" name="password" value="<?= get('password') ? esc(get('password'), 'attr') : '' ?>">
-  </div>
-  <div>
-    <input type="submit" name="login" value="<?= $page->button()->html() ?>">
-  </div>
-</form>
+              </label>
+                  <input type="text" name="password" required="required" aria-required="true" class="form-control" value="<?= get('password') ? esc(get('password'), 'attr') : '' ?>">
+              </li>
+              <li class="submit-buttons">
+                  <input type="submit" value="<?= $page->button()->html() ?>" class="btn btn-primary">
+              </li>
+          </ol>
+      </fieldset>
+  </form>
 
 <?php snippet('footer') ?>
