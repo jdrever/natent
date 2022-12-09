1ce2c1c6fef27e9753beb41fa897695b93ffb09e
James Drever <james@careful.digital>
2022-12-09 16:33:53 +0000

    adding $site variable to guide section controller

--- src/site/controllers/guide-section.php
+++ src/site/controllers/guide-section.php
@@ -1,5 +1,5 @@
 <?php
-return function($kirby, $pages, $page) {
+return function($kirby, $pages, $page, $site) {
     $platform = $kirby->controller('platform' , compact('page', 'pages', 'kirby', 'site'));
     return $platform;
 };
