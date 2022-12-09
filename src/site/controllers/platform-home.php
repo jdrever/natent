8d162160a8177a11ab60311428bba82d6e50204e
James Drever <james@careful.digital>
2022-12-09 15:24:54 +0000

    phase completion on other teams page

--- src/site/controllers/platform-home.php
+++ src/site/controllers/platform-home.php
@@ -3,17 +3,6 @@
 return function($kirby, $pages, $page, $site) 
 {
     $platform = $kirby->controller('platform' , compact('page', 'pages', 'kirby'));
-    $countries=$site->index()->filterBy('template', 'country');
-    if (Cookie::exists('country'))
-    {
-        $country=Cookie::get('country');
-    }
-    else
-    {
-        $language=$kirby->language()->code();    
-        $country=$countries->findBy('language', $language)->title();
-    }
-
     $phases=$site->index()->filterBy('template','phase');
-    return A::merge($platform,compact('countries','country', 'phases'));    
+    return A::merge($platform,compact('phases'));    
 };
--- src/site/controllers/platform.php
+++ src/site/controllers/platform.php
@@ -3,7 +3,7 @@ include 'helpers/DataHelper.php';
 
 use carefulcollab\helpers as helpers;
 
-return function($kirby, $pages, $page) 
+return function($kirby, $pages, $page, $site) 
 {
     $userId=1;
     $team=helpers\DataHelper::getTeamByWPUserId($userId);
@@ -13,8 +13,19 @@ return function($kirby, $pages, $page)
 
     $pointsAdded=$kirby->request()->get('points') ? $kirby->request()->get('points') : 0;
     $pointsAddedOtherTeam=$kirby->request()->get('pointsOther') ? $kirby->request()->get('pointsOther') : 0;
-    //echo(var_dump($team));
-    //die();
-    return [ 'userId' => $userId, 'team' => $team, 'status' =>$status, 'userRole' => $team['role'], 'pointsAdded' => $pointsAdded, 'pointsAddedOtherTeam' =>$pointsAddedOtherTeam ];
+    $countries=$site->index()->filterBy('template', 'country');
+
+    if (Cookie::exists('country'))
+    {
+        $country=Cookie::get('country');
+    }
+    else
+    {
+        $language=$kirby->language()->code();    
+        $country=$countries->findBy('language', $language)->title();
+    }
+
+
+    return [ 'userId' => $userId, 'team' => $team, 'status' =>$status, 'userRole' => $team['role'], 'pointsAdded' => $pointsAdded, 'pointsAddedOtherTeam' =>$pointsAddedOtherTeam, 'country'=>$country, 'countries'=>$countries ];
 }
 ?>
