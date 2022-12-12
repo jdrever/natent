69209d565660a5f5212db7df563ae2bed21961c4
James Drever <james@careful.digital>
2022-12-12 12:58:28 +0000

    fixing margins on for teachers page

--- src/site/templates/for-teachers.php
+++ src/site/templates/for-teachers.php
@@ -1,5 +1,7 @@
 <?php snippet('header') ?>
 <?php snippet ('show-simple-hero') ?>
+<div class="container my-4">
 <?php snippet ('show-blocks') ?>
 <?php snippet('show-sub-pages') ?>
+</div>
 <?php snippet('footer') ?>
