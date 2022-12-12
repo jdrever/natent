967fa260939815b6d443bdafa1dff131f76156aa
James Drever <james@careful.digital>
2022-12-12 12:56:10 +0000

    putting content about sub-pages on teachers page

--- src/site/templates/for-teachers.php
+++ src/site/templates/for-teachers.php
@@ -1,5 +1,5 @@
 <?php snippet('header') ?>
 <?php snippet ('show-simple-hero') ?>
-<?php snippet('show-sub-pages') ?>
 <?php snippet ('show-blocks') ?>
+<?php snippet('show-sub-pages') ?>
 <?php snippet('footer') ?>
