dc438faa14c5711312c24c73174d27bcfcab5731
James Drever <james@careful.digital>
2022-12-18 11:28:22 +0000

    adding simple gallery block

--- src/site/plugins/careful-guides/index.php
+++ src/site/plugins/careful-guides/index.php
@@ -6,6 +6,7 @@ Kirby::plugin(
       'blocks/activity' => __DIR__ . '/blueprints/blocks/activity.yml',
       'blocks/lead-text' => __DIR__ . '/blueprints/blocks/lead-text.yml',
       'blocks/image' => __DIR__ . '/blueprints/blocks/image.yml',
+      'blocks/gallery' => __DIR__ . '/blueprints/blocks/gallery.yml',
       'blocks/heading' => __DIR__ . '/blueprints/blocks/heading.yml',
       'blocks/file' => __DIR__ . '/blueprints/blocks/file.yml',
       'fields/guideContent' => __DIR__ . '/blueprints/fields/guideContent.yml',
@@ -13,6 +14,7 @@ Kirby::plugin(
     ],
     'snippets' => [
       'blocks/image' => __DIR__ . '/snippets/blocks/image.php',
+      'blocks/gallery' => __DIR__ . '/snippets/blocks/gallery.php',
       'blocks/heading' => __DIR__ . '/snippets/blocks/heading.php',
       'blocks/file' => __DIR__ . '/snippets/blocks/file.php',
       'blocks/lead-text' => __DIR__ . '/snippets/blocks/lead-text.php',
---
+++ src/site/plugins/careful-guides/snippets/blocks/gallery.php
@@ -0,0 +1,46 @@
+<?php 
+
+$caption = $block->caption();
+$crop    = $block->crop()->isTrue();
+$ratio   = $block->ratio()->or('auto');
+?>
+<div class="container">
+    <div class="row row-cols-2">
+<?php foreach ($block->images()->toFiles() as $image) : ?>
+        <div class="col">
+<?php
+    $alt     = $image->alt();
+    $caption = $image->caption();
+    //$crop    = $image->crop()->isTrue();
+    $link    = $image->link();
+    //$ratio   = $image->ratio()->or('auto');
+    $src     = null;
+
+    if ($block->fixedWidth()->isNotEmpty())
+    {
+        $image=$image->resize((int)$block->fixedWidth()->value());
+    }
+    $src = $image->url();
+
+    $imgClass="figure-img img-fluid";
+    $figCaptionClass="figure-caption";
+?>
+<?php if ($src): ?>
+<figure<?= Html::attr(['data-ratio' => $ratio, 'data-crop' => $crop], null, ' ') ?>>
+  <?php if ($link->isNotEmpty()): ?>
+  <a href="<?= Str::esc($link->toUrl()) ?>">
+    <img class="<?=$imgClass?>" src="<?= $src ?>" alt="<?= $alt->esc() ?>">
+  </a>
+  <?php else: ?>
+  <img class="<?=$imgClass?>" src="<?= $src ?>" alt="<?= $alt->esc() ?>">
+  <?php endif ?>
+
+  <?php if ($caption->isNotEmpty()): ?>
+  <figcaption class="<?=$figCaptionClass?>">
+    <?= $caption ?>
+  </figcaption>
+  <?php endif ?>
+</figure>
+<?php endif ?>
+        </div>
+<?php endforeach ?>
