926f53da6dbc179433de56b965e3daae8347c457
James Drever <james@careful.digital>
2022-12-18 12:37:50 +0000

    adding backgroundColour back to heading block

--- src/site/plugins/careful-guides/index.php
+++ src/site/plugins/careful-guides/index.php
@@ -10,7 +10,8 @@ Kirby::plugin(
       'blocks/heading' => __DIR__ . '/blueprints/blocks/heading.yml',
       'blocks/file' => __DIR__ . '/blueprints/blocks/file.yml',
       'fields/guideContent' => __DIR__ . '/blueprints/fields/guideContent.yml',
-      'fields/mainContent' => __DIR__ . '/blueprints/fields/mainContent.yml'
+      'fields/mainContent' => __DIR__ . '/blueprints/fields/mainContent.yml',
+      'fields/backgroundColour' => __DIR__ . '/blueprints/fields/backgroundColour.yml',
     ],
     'snippets' => [
       'blocks/image' => __DIR__ . '/snippets/blocks/image.php',
--- src/site/plugins/careful-guides/snippets/blocks/heading.php
+++ src/site/plugins/careful-guides/snippets/blocks/heading.php
@@ -1,18 +1,23 @@
 <?php /** @var \Kirby\Cms\Block $block */ 
 $centreBlock=false;
-$div="";
+$divClass="";
 if ($block->isCentred()->isNotEmpty())
 {
   if ($block->isCentred()->toBool()) 
   {
-    $div='class="mx-auto d-block"';
+    $divClass='mx-auto d-block';
   }
 }
+if ($block->backgroundColour()->isNotEmpty() and $block->backgroundColour() != "#")
+{
+  $divClass.=' has-background';
+  $divStyle='background-color:'. $block->backgroundColour().';';
+}
 ?>
-<?php if (!$div=="") : ?>
-<div <?=$div ?>>
+<?php if (!empty($divClass)) : ?>
+<div class="<?=$divClass ?>" <?php if (!empty($divStyle)) : ?>style="<?=$divStyle?>" <?php endif ?>>
 <?php endif ?>
 <<?= $level = $block->level()->or('h2') ?>><?= $block->text() ?></<?= $level ?>>
-<?php if (!$div=="") : ?>
+<?php if (!empty($divClass)) : ?>
 </div>
 <?php endif ?>
