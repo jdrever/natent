d5079c8ccbbcff8e3ed7c276f2379b280be4f862
James Drever <james@careful.digital>
2022-12-20 12:33:52 +0000

    colour contrast w/label on research questions task

--- src/site/templates/task-share-research-questions.php
+++ src/site/templates/task-share-research-questions.php
@@ -44,7 +44,7 @@
         <input type="hidden"  name="functionNumber<?=$x?>" id="functionNumber<?=$x?>" value="<?=$functionNumber?>">
         <div class="form-floating">
             <input type="text" class="form-control bg-info text-white" name="biologized<?=$x?>" id="biologized<?=$x?>" placeholder="<?=$page->researchQuestionLabel()?> <?=$x?>" aria-label="<?=$page->researchQuestionLabel()?> <?=$x?>" value="<?=$biologisedQuestionName?>">
-            <label for="biologized<?=$x?>"><?=t('Research Question','Research Question')?> <?=$x?></label>
+            <label class="text-white" for="biologized<?=$x?>"><?=t('Research Question','Research Question')?> <?=$x?></label>
         </div>
     </div>
 
