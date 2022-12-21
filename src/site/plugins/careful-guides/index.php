6f14dbb840052faa8ecee84343675f2ea48022c1
James Drever <james@careful.digital>
2022-12-21 13:40:04 +0000

    adding link to commons

--- src/site/plugins/careful-guides/index.php
+++ src/site/plugins/careful-guides/index.php
@@ -11,6 +11,7 @@ Kirby::plugin(
       'blocks/heading' => __DIR__ . '/blueprints/blocks/heading.yml',
       'blocks/file' => __DIR__ . '/blueprints/blocks/file.yml',
       'blocks/google-slide' => __DIR__ . '/blueprints/blocks/google-slide.yml',
+      'blocks/link-to-commons' => __DIR__ . '/blueprints/blocks/link-to-commons.yml',
       'fields/guideContent' => __DIR__ . '/blueprints/fields/guideContent.yml',
       'fields/mainContent' => __DIR__ . '/blueprints/fields/mainContent.yml',
       'fields/backgroundColour' => __DIR__ . '/blueprints/fields/backgroundColour.yml',
@@ -24,6 +25,7 @@ Kirby::plugin(
       'blocks/lead-text' => __DIR__ . '/snippets/blocks/lead-text.php',
       'blocks/quote' => __DIR__ . '/snippets/blocks/quote.php',
       'blocks/google-slide' => __DIR__ . '/snippets/blocks/google-slide.php',
+      'blocks/link-to-commons' => __DIR__ . '/snippets/blocks/link-to-commons.php',
     ]
   ]
 );
---
+++ src/site/plugins/careful-guides/snippets/blocks/link-to-commons.php
@@ -0,0 +1,3 @@
+<a href="<?=$commonsPage->url()?>" class="btn btn-outline-primary">
+  <?= $block->linkText()?>
+</p>
