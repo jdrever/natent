c1b533e614f4b531b8c12e78f749a5ddd9a60354
James Drever <james@careful.digital>
2022-12-21 14:00:49 +0000

    syling add to commons block

--- src/site/plugins/careful-guides/snippets/blocks/link-to-commons.php
+++ src/site/plugins/careful-guides/snippets/blocks/link-to-commons.php
@@ -1,3 +1,6 @@
-<a href="<?=$commonsPage->url()?>" class="btn btn-outline-primary">
+<div class="alert alert-info m-2" role="alert">
+  <i class="bi bi-cc-circle-fill"></i>
+  <a href="<?=$commonsPage->url()?>" class="btn btn-outline-primary">
   <?= $block->linkText()?>
-</p>
+  </a>
+</div>
