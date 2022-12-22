7b2f4ab9f3a55193a01fb5e1e52b87a831b6e3eb
James Drever <james@careful.digital>
2022-12-22 12:11:26 +0000

    adding title to resources page

--- src/site/templates/resources.php
+++ src/site/templates/resources.php
@@ -2,6 +2,7 @@
 <?php snippet('platform-menu') ?>
 <?php snippet('show-status') ?>
 <div class="container my-4">
+<h1 class="pb-2 border-bottom"><?= $page->pageTitle()->isNotEmpty() ? $page->pageTitle() : $page->title() ?></h1>
 <?php snippet ('show-blocks') ?>
 <?php snippet('show-sub-pages') ?>
 </div>
