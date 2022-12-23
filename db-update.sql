ab46572713412d02f7920a7914775ddd7fa54e35
James Drever <james@careful.digital>
2022-12-23 08:27:43 +0000

    test commit -make sure user accounts still work ok

--- db-update.sql
+++ db-update.sql
@@ -6,3 +6,4 @@ ALTER TABLE cc_countries
 UPDATE cc_countries SET language='en', example_team_id=41 WHERE id=1
 
 CREATE TABLE cc_areas_translations (area_id int, language_code varchar(5), name varchar(100), description varchar(250))
+ 
