d5be52ae5525b80b79d599755c359aa114d43e04
James Drever <james@careful.digital>
2023-01-02 17:58:24 +0000

    trying on whole cta block

--- src/site/templates/home.php
+++ src/site/templates/home.php
@@ -12,9 +12,9 @@
     </div>
   </div>
 </div>
-<div class="container my-4">
+<div class="container my-4" style="background-color:#f9f8c8;">
   <?php snippet('show-blocks')?>
-  <div class="container mx-auto" style="width:60%;background-color: #f9f8c8;" >
+  <div class="container mx-auto" style="width:60%;" >
     <h3 class="text-center fw-bold mt-4"><?=$page->getStartedHeader()?></h3>
     <p class="d-flex flex-row justify-content-center mb-3">
 <?php if ($forTeachers) : ?>
