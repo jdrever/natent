3bfd89e81576937f55126ea23439e146afb34a4b
James Drever <james@careful.digital>
2023-01-02 17:57:04 +0000

    trying yellow block on home page

--- src/site/templates/home.php
+++ src/site/templates/home.php
@@ -14,7 +14,7 @@
 </div>
 <div class="container my-4">
   <?php snippet('show-blocks')?>
-  <div class="container mx-auto" style="width:60%;">
+  <div class="container mx-auto" style="width:60%;background-color: #f9f8c8;" >
     <h3 class="text-center fw-bold mt-4"><?=$page->getStartedHeader()?></h3>
     <p class="d-flex flex-row justify-content-center mb-3">
 <?php if ($forTeachers) : ?>
