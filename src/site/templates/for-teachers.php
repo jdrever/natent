d101326330a0def69cd1a2f0deaf13815814d387
James Drever <james@careful.digital>
2023-01-02 17:19:30 +0000

    for teachers header to match for students

--- src/site/templates/for-teachers.php
+++ src/site/templates/for-teachers.php
@@ -1,5 +1,17 @@
 <?php snippet('header') ?>
-<?php snippet ('show-simple-hero') ?>
+<div class="container-fluid bg-dark text-light p-4"
+  style="background-image: url('/assets/images/SDGs.png'); background-repeat: no-repeat;background-size:cover; height:250px;">
+  <div class="col-lg-8 mx-auto mask mt-0">
+    <div class="shrinkwrap text-start text-white">
+      <h1>
+        <span><?=$page->heroHeader()?></span>
+      </h1>
+      <h2>
+        <span><?=$page->heroContent()?></span>
+      </h2>
+    </div>
+  </div>
+</div>
 <div class="container my-4">
 <?php snippet ('show-blocks') ?>
 <?php snippet('show-sub-pages') ?>
