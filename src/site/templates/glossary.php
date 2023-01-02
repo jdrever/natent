fa8ba7896dad8759cd3c73d34959e051f7ff2065
James Drever <james@careful.digital>
2023-01-02 16:17:58 +0000

    add icon to glossary page

--- src/site/templates/glossary.php
+++ src/site/templates/glossary.php
@@ -3,7 +3,7 @@
 <?php snippet('breadcrumb') ?>
 <?php snippet('show-status') ?>
 <div class="container my-4">
-<h1 class="pb-2 border-bottom"><?= $page->pageTitle()->isNotEmpty() ? $page->pageTitle() : $page->title() ?></h1>
+<h1 class="pb-2 border-bottom"><i class="bi bi-list-columns-reverse"></i> <?= $page->pageTitle()->isNotEmpty() ? $page->pageTitle() : $page->title() ?></h1>
 <?php snippet ('show-blocks') ?>
 <?php snippet('show-glossary') ?>
 </div>
