af5b1e5faf9a75b83dd70e275b8d4618ad8d16bf
James Drever <james@careful.digital>
2023-01-05 11:48:57 +0000

    making platform home layout more compact

--- src/site/snippets/show-phases.php
+++ src/site/snippets/show-phases.php
@@ -1,18 +1,19 @@
 <?php 
 use carefulcollab\helpers as helpers;
 
-$phaseNumber=1;
 $width=300;
 $height=200;
 
-$phaseCol=$userLoggedIn ? 1 : 2;
+$phaseCol=4;
 $hasPhases=false;
+
 ?>
 
-<div class="container p-3" style="background-color:lemonchiffon;">
-  <h2><?=t('Get Started!', 'Get Started!')?></h2>
+<div class="container p-3">
+
   <div class="container">
     <div class="row">
+      <div class="col"><h2><?=t('Welcome!', 'Welcome!')?></h2></div>
       <div class="col">
         <?=t('Select Your Country','Select Your Country')?>: <?php snippet('country-switcher') ?>
       </div>
@@ -21,39 +22,25 @@ $hasPhases=false;
         <a href="<?=Cookie::get('resumePage')?>" class="btn btn-primary m-2"><?=t('RESUME','RESUME')?> <i
             class="bi bi-arrow-right"></i></a>
         <?php else : ?>
-        <a href="<?=$countryPhase->url()?>" class="btn btn-primary m-2"><?=t('GET STARTED', 'GET STARTED')?> →</a>
+        <a href="<?=$countryPhases[0]->url?>" class="btn btn-primary m-2"><?=t('GET STARTED', 'GET STARTED')?> →</a>
         <?php endif ?>
       </div>
     </div>
   </div>
-  <p><strong><?=t('Work through each Phase below in order','Work through each Phase below in order')?>.</strong></p>
 </div>
-<div class="container">
-  <div class="row row-cols-<?=$phaseCol?>">
-
-<?php foreach ($phases as $phase) : 
-    $pagesInPhase=$page->index()->filterBy('phase', strtolower($phase->title()));
-    if ($pagesInPhase):
-      $countryPhase=$pagesInPhase->filterBy('countries', '*=', str_replace(" ","-",strtolower($country)))->first();
-      if ($countryPhase) :
-        $phaseCompletionInfo=helpers\DataHelper::getCompletionByPhaseTypeForTeam($team['id'],$phase->title());
-        $phaseCompletion = $phaseCompletionInfo['percent_complete']; ?>
-    <div class="col p-3 m-1" <?php if ($phase->backgroundColour()->isNotEmpty()) : ?>style="background-color: <?=$phase->backgroundColour()?>" <?php endif ?>>
-      <div class="d-grid gap-2">
-      <a href="<?=$countryPhase->url()?>" class="btn btn-outline-primary">
-        <span class="h2"><i class="bi bi-<?=$phaseNumber?>-circle"></i> <?=$phase->title()?></span>
-        <span class="badge bg-info"><?= $phaseCompletion ?>%</span>
+<div class="container p-2">
+<p><strong><?=t('Work through each Phase below in order','Work through each Phase below in order')?>.</strong></p>
+<div class="row">
+<?php foreach ($countryPhases as $phase) : ?>
+      <div class="col">
+        <div class="d-grid gap-2">
+      <a href="<?=$phase->url?>" class="btn btn-outline-primary" <?php if ($phase->backgroundColour) : ?>style="background-color: <?=$phase->backgroundColour?>" <?php endif ?>>
+        <span class="h4"><i class="bi bi-<?=$phase->phaseNumber?>-circle"></i><br><?=$phase->title?></span>
+        <br><span class="badge bg-info"><?= $phase->phaseCompletion ?>%</span>
       </a>
+        </div>
       </div>
-    </div>
-    <?php
-      $hasPhases=true;
-    endif;
-  endif;  
-  $phaseNumber++; 
-endforeach; 
-if ($hasPhases) :
-?>
-  </div>
+    <?php endforeach ?>
 </div>
-<?php endif ?>
+</div>
+
