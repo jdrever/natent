13cff2c4fb1b3342db4ef9fad24da49f6ace7e30
James Drever <james@careful.digital>
2023-01-06 14:03:37 +0000

    adding active page to side menu

--- src/site/snippets/show-guide-contents.php
+++ src/site/snippets/show-guide-contents.php
@@ -9,19 +9,19 @@ if(!isset($contents)) $contents=$page->children()->listed();
     <div class="list-group-item list-group-item-dark"><?=$section->title()?></div>
   <?php endif ?>
   <?php if ($section->blueprint()->name()=='pages/guide-section') : ?>
-    <a href="<?=$section->url()?>" class="list-group-item">
+    <a href="<?=$section->url()?>" class="list-group-item<?php e($section->isOpen(), ' active') ?>"<?php e($section->isOpen(), 'aria-current="true"') ?>>
     <i class="bi bi-journal-text"></i>
      <?=$section->title()?>
   </a>
   <?php endif ?>
   <?php if ($section->blueprint()->name()=='pages/guide-activity') : ?>
-    <a href="<?=$section->url()?>" class="list-group-item">
+    <a href="<?=$section->url()?>" class="list-group-item<?php e($section->isOpen(), ' active') ?>"<?php e($section->isOpen(), 'aria-current="true"') ?>>
     <i class="bi bi-activity"></i>
      <?=$section->title()?>
   </a>
   <?php endif ?>
   <?php if (str_starts_with($section->blueprint()->name(),'pages/task')) : ?>
-    <a href="<?=$section->url()?>" class="list-group-item">
+    <a href="<?=$section->url()?>" class="list-group-item<?php e($section->isOpen(), ' active') ?>"<?php e($section->isOpen(), 'aria-current="true"') ?>>
     <i class="bi bi-list-check"></i>
      <?=$section->title()?>
   </a>
