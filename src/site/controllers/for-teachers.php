4df1edf3f52eaf733a5d77c13a0a57dc62bad3e8
James Drever <james@careful.digital>
2023-01-06 16:39:40 +0000

    stopping students/teachers pages needing login

--- src/site/controllers/for-teachers.php
+++ src/site/controllers/for-teachers.php
@@ -1,7 +1,7 @@
 <?php
 return function($kirby, $pages, $page, $site) 
 {
-    $requiresLogin=true;
+    $requiresLogin=false;
     $isNonLearningJourneyPage=true;
     $registerPage=site()->find('register-your-school');
     return $kirby->controller('platform' , compact('kirby', 'pages', 'page', 'site', 'requiresLogin', 'isNonLearningJourneyPage', 'registerPage'));
