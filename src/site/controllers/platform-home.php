c4eaf58a293b7f2c6d1c4d4e7392cd3e3bb8b38c
James Drever <james@careful.digital>
2023-01-12 11:28:33 +0100

    tidying code for county/language mess

--- src/site/controllers/platform-home.php
+++ src/site/controllers/platform-home.php
@@ -23,32 +23,23 @@ return function ($kirby, $pages, $page, $site)
 
     $languagePage=$page->children()->filterBy('template','country')->filterBy('language','*=', $language)->first();
 
-    //echo(var_dump($languagePage));
-
     foreach ($phases as $phase)
     {
 
         $phasePage = $languagePage->index()->filterBy('phase', strtolower($phase->title()))->first();
-        //echo(var_dump($phasePage));
         if ($phasePage)
         {
-            //$countryPhase = $pagesInPhase->filterBy('countries', '*=', str_replace(" ", "-", strtolower($country)))->first();
-            //if ($countryPhase)
-            //{
-                $phaseCompletionInfo = helpers\DataHelper::getCompletionByPhaseTypeForTeam($team['id'], $phase->title());
-                $addPhase=new StdClass;
-                $addPhase->phaseNumber=$phaseNumber+1;
-                $addPhase->phaseCompletion=$phaseCompletionInfo['percent_complete'];
-                $addPhase->title=$phasePage->title();
-                $addPhase->url=$phasePage->url();
-                $addPhase->backgroundColour=$phase->backgroundColour();
-                $countryPhases[]=$addPhase;
-                $phaseNumber++;
-            //}
+            $phaseCompletionInfo = helpers\DataHelper::getCompletionByPhaseTypeForTeam($team['id'], $phase->title());
+            $addPhase=new StdClass;
+            $addPhase->phaseNumber=$phaseNumber+1;
+            $addPhase->phaseCompletion=$phaseCompletionInfo['percent_complete'];
+            $addPhase->title=$phasePage->title();
+            $addPhase->url=$phasePage->url();
+            $addPhase->backgroundColour=$phase->backgroundColour();
+            $countryPhases[]=$addPhase;
+            $phaseNumber++;
         }
     }
-
-    //echo(var_dump($countryPhases));
 
     return A::merge($platform, compact('countryPhases', 'latestComments', 'latestAppreciations'));
 };
