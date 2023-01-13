76424e0f6aa662d18a6ac2a25f50b9b29e1decf4
James Drever <james@careful.digital>
2023-01-13 16:07:11 +0100

    testing file embed

--- src/site/plugins/careful-guides/snippets/blocks/file.php
+++ src/site/plugins/careful-guides/snippets/blocks/file.php
@@ -2,6 +2,7 @@
 
 <div class="container m-2 p-2 bg-light">
     <?php if($file = $block->file()->toFile()): ?>
+        <p>ext=<?=$file->extension()?> embed=<?=block->showEmbed()->toBool()?></p>
         <?php if ($file->extension()=='pdf'&&$block->showEmbed()->toBool()!=false) : ?>
             <iframe src="https://docs.google.com/viewer?url=<?=$file->url()?>&embedded=true" frameborder="0" height="300px" width="100%"></iframe>
         <?php endif ?>    
