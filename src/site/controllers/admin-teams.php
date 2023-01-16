7a2a4867a1654acc8baf386e7ed0d67ae3e2f95a
James Drever <james@careful.digital>
2023-01-16 14:12:57 +0000

    adding modal to team creation process

--- src/site/controllers/admin-teams.php
+++ src/site/controllers/admin-teams.php
@@ -1,83 +1,88 @@
 <?php
+
 use carefulcollab\helpers as helpers;
 
-return function($kirby, $pages, $page, $site) {
-    $requiresLogin = true;
-    $platform = $kirby->controller('platform' , compact('page', 'pages', 'kirby', 'site', 'requiresLogin'));
-    $team = $platform['team'];
-    $userId=$team['user_id'];
+return function ($kirby, $pages, $page, $site)
+{
+  $requiresLogin = true;
+  $platform = $kirby->controller('platform', compact('page', 'pages', 'kirby', 'site', 'requiresLogin'));
+  $team = $platform['team'];
+  $userId = $team['user_id'];
+  $result = '';
 
-    $country = $platform['country'];
+  $country = $platform['country'];
 
-    $newPassword=random_str(8);
-    
-    if($kirby->request()->is('POST')) 
+  $newPassword = helpers\DataHelper::random_str(8);
+
+  if ($kirby->request()->is('POST'))
+  {
+    $action = $_POST['action'];
+
+    if ($action === 'CREATE-TEAM')
     {
-        try {
-          $teamName=$_POST['teamName'];
+      try
+      {
+        $teamName = $_POST['teamName'];
 
-            $user = $kirby->users()->create([
-              'name'      => $teamName,
-              'email'     => str_replace(' ','-',$teamName).'@natent.eu',
-              'password'  => get('password'),
-              'language'  => 'en',
-              'role'      => 'team',
-              'content'   => [
-                'birthdate' => '1989-01-29'
-              ]
-            ]);
+        $user = $kirby->users()->create([
+          'name'      => $teamName,
+          'email'     => str_replace(' ', '-', $teamName) . '@natent.eu',
+          'password'  => get('password'),
+          'language'  => 'en',
+          'role'      => 'team',
+          'content'   => [
+            'birthdate' => '1989-01-29'
+          ]
+        ]);
 
-            
-            //TODO: absolutely can't hard code this
-            $locationId=2;
-            $result=helpers\DataHelper::addTeam($userId, $teamName, $locationId);
-          
-            //echo 'The user "' . $user->name() . '" has been created';
-            $result=true;
-          
-          } catch(Exception $e) {
-          
-            echo 'The user could not be created';
-            $result=false;
-            echo($e->getMessage());
-          
-          }
-          $teams = helpers\DataHelper::getTeamsByLocation($userId);
-          return A::merge($platform, compact('newPassword','result', 'teams'));
+
+        //TODO: absolutely can't hard code this
+        $locationId = 2;
+        $result = helpers\DataHelper::addTeam($userId, $teamName, $locationId);
+
+        //echo 'The user "' . $user->name() . '" has been created';
+        $result = true;
+      }
+      catch (Exception $e)
+      {
+
+        echo 'The team could not be created';
+        $result = false;
+        echo ($e->getMessage());
+      }
     }
-    else
+    if ($action == "RENAME-TEAM")
     {
-        $teams = helpers\DataHelper::getTeamsByLocation($userId);
-        return A::merge($platform, compact('newPassword', 'teams'));
+      $actionType = "Team name update";
+      $teamId = $_POST['teamId'];
+      $teamName = $_POST['teamName'];
+      $oldTeamName = $_POST['teamName'];
+      $team=$kirby->user(str_replace(' ', '-', $oldTeamName) . '@natent.eu');
+      $result = helpers\DataHelper::updateTeamName($userId, $teamId, $teamName);
+      $team->changeName($teamName);
+      $team->changeEmail(str_replace(' ', '-', $teamName) . '@natent.eu');
     }
+
+    if ($action == "RESET-PASSWORD")
+    {
+      $actionType = "Reset team password";
+      $teamName = $_POST['teamName'];
+      $password = $_POST['password'];
+      $team=$kirby->user(str_replace(' ', '-', $teamName) . '@natent.eu');
+      $team->changePassword($password);
+    }
+
+    if ($action == "REMOVE-TEAM")
+    {
+      $actionType = "Team removal";
+      $teamId = $_POST['teamId'];
+      $teamName = $_POST['teamName'];
+      $result = helpers\DataHelper::removeTeam($userId, $teamId);
+      $team=$kirby->user(str_replace(' ', '-', $teamName) . '@natent.eu');
+      $team->delete();
+    }
+  }
+
+  $teams = helpers\DataHelper::getTeamsByLocation($userId);
+  return A::merge($platform, compact('newPassword', 'result', 'teams'));
 };
-
-/**
- * Generate a random string, using a cryptographically secure 
- * pseudorandom number generator (random_int)
- *
- * This function uses type hints now (PHP 7+ only), but it was originally
- * written for PHP 5 as well.
- * 
- * For PHP 7, random_int is a PHP core function
- * For PHP 5.x, depends on https://github.com/paragonie/random_compat
- * 
- * @param int $length      How many characters do we want?
- * @param string $keyspace A string of all possible characters
- *                         to select from
- * @return string
- */
-function random_str(
-  int $length = 64,
-  string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-): string {
-  if ($length < 1) {
-      throw new \RangeException("Length must be a positive integer");
-  }
-  $pieces = [];
-  $max = mb_strlen($keyspace, '8bit') - 1;
-  for ($i = 0; $i < $length; ++$i) {
-      $pieces []= $keyspace[random_int(0, $max)];
-  }
-  return implode('', $pieces);
-}
--- src/site/snippets/admin/teams.php
+++ src/site/snippets/admin/teams.php
@@ -1,3 +1,7 @@
+<?php
+use carefulcollab\helpers as helpers;
+?>
+
 <form>
   <label>Country</label>
   <select>
@@ -12,32 +16,55 @@
 <div class="container m-2 border border-primary bg-light p-3">
   <h2>Create a New Team</h2>
 
-  <form method="post">
+  <form method="post" id="createTeamForm" name="createTeamForm">
+    <input type="hidden" id="action" name="action" value="CREATE-TEAM">
     <div class="mb-3">
       <label for="teamName" class="form-label">Team Name</label>
-      <input type="text" class="form-control" id="teamName" name="teamName" aria-describedby="teamNameHelp">
+      <input type="text" class="form-control" id="teamName" name="teamName" aria-describedby="teamNameHelp" required>
       <div id="teamNameHelp" class="form-text"><span style="font-size: 1.2em;">Please <strong>don't use anyone's real names!</strong></span></div>
     </div>
     <div class="mb-3">
       <label for="teamPasword" class="form-label">Password</label>
-      <input type="text" class="form-control" id="teamPassword" name="teamPassword" value="<?=$newPassword?>" aria-describedby="passwordHelp">
+      <input type="text" class="form-control" id="teamPassword" name="teamPassword" value="<?=$newPassword?>" aria-describedby="passwordHelp" required readonly>
       <div id="passwordHelp" class="form-text"><span style="font-size: 1.2em;">You need to <strong>note this password down NOW</strong> - you won't get to see it again!</span></div>
     </div>
-    <button type="submit" class="btn btn-primary">CREATE TEAM</button>
+    <button type="submit" name="createTeamButton" id="createTeamButton" class="btn btn-primary">CREATE TEAM</button>
   </form>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="passwordModal" tabindex="-1" aria-labelledby="passwordModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="passwordModalLabel">Make a note of this password</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        You need to note this password down NOW - you won't get to see it again!
+        <br><br>
+        Password: <?=$newPassword?> 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">CANCEL</button>
+        <button type="button" class="btn btn-primary" id="createTeamModalButton" name="createTeamModalButton">CREATE TEAM</button>
+      </div>
+    </div>
+  </div>
 </div>
 <h2>Existing Teams</h2>
 
 <table class="table m-2 p-2 border">
     <tr>
         <th><?= t("Team Name","Team Name") ?></th>
-        <th><?= t("Reset Password","Reset Password") ?></th>
+        <th><?= t("Reset Password To","Reset Password To") ?></th>
         <th><?= t("Remove","Remove") ?></th>
     </tr>
 
     <?php
     foreach ($teams as $thisTeam)
     {
+      $newResetPassword=helpers\DataHelper::random_str(8);
     ?>
         <tr>
             <td>
@@ -45,15 +72,16 @@
                     <input type="hidden" id="action" name="action" value="RENAME-TEAM">
                     <input type="text" id="teamName" name="teamName" value="<?= $thisTeam['name'] ?>" required>
                     <input type="hidden" id="teamId" name="teamId" value="<?= $thisTeam['id'] ?>">
+                    <input type="hidden" id="oldTeamName" name="oldTeamName" value="<?= $thisTeam['name'] ?>">
                     <input type="submit" class="btn btn-primary" value="<?= t("RENAME","RENAME") ?>">
                 </form>
             </td>
             <td>
                 <form action="<?= $_SERVER['REQUEST_URI'] ?>" method="post">
                     <input type="hidden" id="action" name="action" value="RESET-PASSWORD">
-                    <input type="text" id="password" name="password" value="<?= $newPassword ?>" required aria-describedby="passwordHelp">
+                    <input type="text" id="password" name="password" value="<?= $newResetPassword ?>" required aria-describedby="passwordHelp" readonly>
                     <div id="passwordHelp" class="form-text"><span style="font-size: 1.2em;"><strong>Note this password down NOW before clicking RESET</strong></span></div>
-                    <input type="hidden" id="teamId" name="teamId" value="<?= $thisTeam['id'] ?>">
+                    <input type="hidden" id="teamName" name="teamName" value="<?= $thisTeam['name'] ?>">
                     <input type="submit" class="btn btn-primary" value="<?= t("RESET","RESET") ?>">
                 </form>
             </td>
@@ -75,6 +103,7 @@
                         <label for="teamCheck<?= $thisTeam['id'] ?>"><?= t("Type the Team name to confirm","Type the Team name to confirm") ?></label>
                     </div>
                     <input type="hidden" id="teamId" name="teamId" value="<?= $thisTeam['id'] ?>">
+                    <input type="hidden" id="teamName" name="teamName" value="<?= $thisTeam['name'] ?>">
                     <input type="submit" class="btn btn-danger" value="<?= t("REMOVE TEAM AND ALL OF THEIR WORK","REMOVE TEAM AND ALL OF THEIR WORK") ?>">
                 </form>
                 <?php
@@ -86,3 +115,39 @@
     }
     ?>
 </table>
+
+<script>
+
+let createTeamButton=document.getElementById('createTeamButton');
+
+let createTeamForm=document.getElementById('createTeamForm');
+let createTeamModalButton=document.getElementById('createTeamModalButton');
+
+const passwordModal = new bootstrap.Modal('#passwordModal');
+
+createTeamButton.addEventListener('click', function(event)
+{
+  
+  passwordModal.show();
+  
+  event.preventDefault();
+});
+
+
+createTeamModalButton.addEventListener('click', function(event)
+{
+  // Check if valid using HTML5 checkValidity() builtin function
+  if (createTeamForm.checkValidity()) 
+  {
+    console.log('valid');
+    createTeamForm.submit();
+  } 
+  
+  else {
+    passwordModal.hide();
+    createTeamForm.reportValidity();
+    console.log('not valid');
+  }
+});
+
+</script>
