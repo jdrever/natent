23cc2aa704544e7eeead3d0c89370c098d22841e
James Drever <james@careful.digital>
2023-01-17 14:03:46 +0000

    sub pages as buttons snippet

--- src/site/snippets/show-sub-pages-as-buttons.php
+++ src/site/snippets/show-sub-pages-as-buttons.php
@@ -6,11 +6,13 @@ $subPages=$page->children()->filter(function ($child) {
 
 if (count($subPages)>0) :
 ?>
-<div class="container px-4" id="featured-3">
+<div class="container px-4">
   <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
     <?php foreach($subPages as $subPage): ?>
-    <div class="feature col">
-      <a href="<?= $subPage->url() ?>" class="btn btn-lg btn-outline-secondary"><?= $subPage->pageTitle()->isEmpty() ? $subPage->title()->html() : $subPage->pageTitle()->html() ?></a>
+    <div class="col">
+      <div class="d-grid gap-2">
+        <a href="<?= $subPage->url() ?>" class="btn btn-lg btn-primary btn-block"><?= $subPage->pageTitle()->isEmpty() ? $subPage->title()->html() : $subPage->pageTitle()->html() ?></a>
+      </div>
     </div>
     <?php endforeach ?>
   </div>
