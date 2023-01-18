e58223116e4cd92da3c952d1d0f208d51078be9a
James Drever <james@careful.digital>
2023-01-18 12:56:30 +0000

    more debugging

--- src/site/controllers/admin-teachers.php
+++ src/site/controllers/admin-teachers.php
@@ -128,7 +128,7 @@ return function ($kirby, $pages, $page, $site)
     $adminLocation=$team['location_id'];
   }
 
-  echo('adminLocation='.$adminLocation);
+  echo('team='.var_dump($team));
 
   $locations=($adminCountry>0) ? helpers\DataHelper::getLocationsByCountryId($adminCountry) : [];
   $teachers = helpers\DataHelper::getTeachersByLocationId($adminLocation);
