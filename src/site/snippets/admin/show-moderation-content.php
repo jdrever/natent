72d4eddebe603437d479ff17413d28e2d7617bbe
James Drever <james@careful.digital>
2023-01-19 12:05:18 +0000

    debug moderation queue error

--- src/site/snippets/admin/show-moderation-content.php
+++ src/site/snippets/admin/show-moderation-content.php
@@ -1,6 +1,8 @@
 <?php
 use carefulcollab\helpers as helpers;
   $content=helpers\DataHelper::getModerationContent($contentType, $contentId);
+  echo(var_dump($content)); 
+  die();
     if ($contentType=="Team Profile")
     {
  ?>
