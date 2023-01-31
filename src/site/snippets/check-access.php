81cc7d332c52cc280aef4e94bd6614a387092c8d
James Drever <james@careful.digital>
2023-01-31 09:07:37 +0000

    changing default text for access language var

--- src/site/snippets/check-access.php
+++ src/site/snippets/check-access.php
@@ -5,7 +5,7 @@ if (!isset($showExampleTeamText)) $showExampleTeamText=true;
 <?php if (!$userLoggedIn) : ?>
 <div class="alert alert-info p-2" role="alert">
   <h2><i class="bi bi-info-square-fill"></i> <?=t("Register to access collaboration features","Register to access collaboration features")?></h2>
-  <p><?=t("Everyone can explore the Natural Entrepreurs Learning Journey","Everyone can explore the Natural Entrepreurs Learning Journey but to complete tasks and collaborate with other teams, you will need to register your school with the project")?>.</p>
+  <p><?=t("Everyone can explore the Natural Entrepreurs Learning Journey","Everyone can explore the Natural Entrepreurs Learning Journey")?>.</p>
   <p><a class="btn btn-primary m-2" href="<?=$registerPage->url()?>"><?=$registerPage->title()?></a></p>
   <p><?=t("Or if you already have a username and password, then","Or if you already have a username and password, then")?> <a class="btn btn-primary" href="<?=$loginPage->url()?>"><?=$loginPage->title()?></a></p>
 
