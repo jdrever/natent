6dad3111dd7ba0e82b3fccb85303edbc37b15046
James Drever <james@careful.digital>
2023-01-31 09:25:56 +0000

    adding latvian to language vars

--- src/site/languages/en.php
+++ src/site/languages/en.php
@@ -258,6 +258,7 @@ return [
         'Hungarian' => 'Hungarian',
         'Netherlands' => ' Netherlands',
         'Romanian' => 'Romanian',
+        'Latvian' => 'Latvian',
     ],
     'url' => NULL
 ];
