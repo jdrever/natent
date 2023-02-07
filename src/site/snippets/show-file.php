db0c704817a4d77cd2c6132a7c7f76e4f82caa04
James Drever <james@careful.digital>
2023-02-07 16:10:38 +0000

    stop embed pdf showing as not always working

--- src/site/snippets/show-file.php
+++ src/site/snippets/show-file.php
@@ -5,7 +5,7 @@ if(preg_match('(jpg|jpeg|png|gif)', $fileExt) === 1) :?>
   <a target="_blank" href="<?=$fileUrl ?>"><img class="img-fluid" src="<?=$fileUrl ?>" alt="<?=$altText?>" /></a>
 <?php endif;
 if ($fileExt=="pdf") : ?>
-  <iframe src="https://docs.google.com/viewer?url=<?=$fileUrl?>&embedded=true" frameborder="0" height="300px" width="100%"></iframe>
+  <!--<iframe src="https://docs.google.com/viewer?url=<?=$fileUrl?>&embedded=true" frameborder="0" height="300px" width="100%"></iframe>-->
   <a target="_blank" href="<?=$fileUrl ?>" class="btn btn-outline-primary btn-outline btn-sm"><?=t('DOWNLOAD','DOWNLOAD')?></a>
 <?php endif ?>
 </div>
