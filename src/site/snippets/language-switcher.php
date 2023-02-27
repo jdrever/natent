4ec63273737e2cc0612f73892b32e392fc1c1f24
James Drever <james@careful.digital>
2023-02-27 10:58:24 +0000

    translating language switcher

--- src/site/snippets/language-switcher.php
+++ src/site/snippets/language-switcher.php
@@ -1,12 +1,12 @@
 <div class="dropdown">
   <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-    <?=$kirby->language()->name()?>
+    <?=t($kirby->language()->name(),$kirby->language()->name())?>
   </a>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
     <?php foreach($kirby->languages() as $language): ?>
     <a class="dropdown-item <?php e($kirby->language() == $language, ' active') ?>"
       href="<?php e($page->translation($language->code())->exists(), $page->url($language->code()), $language->url()) ?>" hreflang="<?php echo $language->code() ?>">
-      <?php echo html($language->name()) ?>
+      <?=t($language->name(),$language->name()) ?>
     </a>
     <?php endforeach ?>
   </div>
