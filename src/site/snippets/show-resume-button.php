ed952d766dbfcd0f19bed51802b0abc6c71d825e
James Drever <james@careful.digital>
2023-05-13 09:18:13 +0100

    correcting spelling on resume button

--- src/site/snippets/show-resume-button.php
+++ src/site/snippets/show-resume-button.php
@@ -2,6 +2,6 @@
 if ($userLoggedIn&&$isNonLearningJourneyPage==true&&Cookie::exists('resumePage')) :
 ?>
 <div class="d-flex justify-content-end">
-    <a href="<?=Cookie::get('resumePage')?>" class="btn btn-info btn-sml"><i class="bi bi-arrow-left"></i> <?=t('RESUME YOUR LEARING JOURNEY','RESUME YOUR LEARING JOURNEY')?></a>  
+    <a href="<?=Cookie::get('resumePage')?>" class="btn btn-info btn-sml"><i class="bi bi-arrow-left"></i> <?=t('RESUME YOUR LEARNING JOURNEY','RESUME YOUR LEARNING JOURNEY')?></a>  
 </div>
 <?php endif ?>
