e70bb0cad3c4404eac65077ee3e33000f9a8cc8d
James Drever <james@careful.digital>
2023-06-01 12:05:14 +0100

    adding remaining translations for team comments

--- src/site/snippets/show-team-comments.php
+++ src/site/snippets/show-team-comments.php
@@ -2,7 +2,7 @@
 <p class="p-1 border">
   <small>
     <i class="bi bi-chat-fill"></i>
-    on your <?=$comment['content_type']?> by
+    <?=t('on your','on your')?> <?=t($comment['content_type'],$comment['content_type'])?> by
     <a href="/other-team-page/?teamId=<?=$comment['team_id']?>"><?=$comment['team_name']?></a>
     <br>
     <i class="bi bi-quote"></i>
