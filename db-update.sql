3d169174d0c140da1e53242b1424f5e190bc4e70
James Drever <james@careful.digital>
2023-08-25 08:42:10 +0100

    tidying up doc root

--- db-update.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE cc_countries 
-    ADD language varchar(50),
-    ADD example_team_id int,
-
-
-UPDATE cc_countries SET language='en', example_team_id=41 WHERE id=1
-
-CREATE TABLE cc_areas_translations (area_id int, language_code varchar(5), name varchar(100), description varchar(250))
- 
